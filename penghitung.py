0aa85aa8c693bb6a44face59c89da8e444ab4a2c
Fälly-tly <akunsekolahku1026@gmail.com>
2025-11-18 07:24:55 +0700

    Create penghitung.py

---
+++ penghitung.py
@@ -0,0 +1,6 @@
+nama = input("Siapa namamu? ")
+tahun_lahir = int(input("Tahun lahirmu? "))
+tahun_sekarang = 2025
+umur = tahun_sekarang - tahun_lahir
+print("Halo, " + nama)
+print("Umurmu " + str(umur) + " tahun")
